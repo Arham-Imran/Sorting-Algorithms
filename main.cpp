57dd8e30497d4b0b9b1439fce34c876299cbd11f
Arham_Imran <marhamimran.mts42@gmail.com>
2024-09-11 15:33:12 +0500

    added random array in main()

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
-int main(int, char**){
-    std::cout << "Hello, from Sorting_Algortihms!\n";
+int main(void)
+{
+    srand(0);
+    int arr[10] = {};
+    for(int i=0; i<sizeof(arr)/sizeof(int); i++)
+        arr[i] = rand() % 26;
+    std::cout << std::endl;
+    
+    for(int i=0; i<sizeof(arr)/sizeof(int); i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+
+    for(int i=0; i<sizeof(arr)/sizeof(int); i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
 }
