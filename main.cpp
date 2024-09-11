18038550f820a2f8a6fda79fd76278068692b23a
Arham_Imran <marhamimran.mts42@gmail.com>
2024-09-11 16:13:25 +0500

    fixed alot of bugs XD

--- main.cpp
+++ main.cpp
@@ -2,24 +2,25 @@
 
 void bubbleSort(int* arr, int size)
 {
-    for(int i=0; i<size-1; i++)
+    for(int i=0; i<size; i++)
     {
-        for(int i=0; i<size-1; i++)
+        for(int j=0; j<size-1; j++)
         {
-            if(arr[i] > arr[i+1])
+            if(arr[j] > arr[j+1])
             {
-                arr[i+1] += arr[i];
-                arr[i] = arr[i+1] - arr[i];
-                arr[i+1] -= arr[i]; 
+                arr[j+1] += arr[j];
+                arr[j] = arr[j+1] - arr[j];
+                arr[j+1] -= arr[j];
             }
         }
         size--;
+        i = 0;
     }
 }
 
 int main(void)
 {
-    srand(0);
+    srand(time(0));
     int arr[10] = {};
     for(int i=0; i<sizeof(arr)/sizeof(int); i++)
         arr[i] = rand() % 26;
