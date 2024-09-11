c55de4e7015e8d04cbb6651b60a3296054275d20
Arham_Imran <marhamimran.mts42@gmail.com>
2024-09-11 18:49:57 +0500

    implemented recursive bubble sort

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <bits/stdc++.h>
 
+void RecursiveBubbleSort(int* arr, int size)
+{
+    if(size == 1)
+        return;
+
+    for(int i=0; i<size-1; i++)
+    {
+        if(arr[i] > arr[i+1])
+        {
+            arr[i+1] += arr[i];
+            arr[i] = arr[i+1] - arr[i];
+            arr[i+1] -= arr[i];
+        }
+    }
+
+    RecursiveBubbleSort(arr, --size);
+}
+
 int main(void)
 {
     srand(time(0));
@@ -11,6 +29,8 @@ int main(void)
     for(int i=0; i<sizeof(arr)/sizeof(int); i++)
         std::cout << arr[i] << " ";
     std::cout << std::endl;
+
+    RecursiveBubbleSort(arr, sizeof(arr)/sizeof(int));
 
     for(int i=0; i<sizeof(arr)/sizeof(int); i++)
         std::cout << arr[i] << " ";
