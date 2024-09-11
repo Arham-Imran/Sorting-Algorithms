650dcbb076e8095adeb5217285402959903ff067
Arham_Imran <marhamimran.mts42@gmail.com>
2024-09-11 15:26:58 +0500

    implemented bubble sort

--- main.cpp
+++ main.cpp
@@ -1,5 +1,36 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
-int main(int, char**){
-    std::cout << "Hello, from Sorting_Algortihms!\n";
+void bubbleSort(int* arr, int size)
+{
+    for(int i=0; i<size-1; i++)
+    {
+        for(int i=0; i<size-1; i++)
+        {
+            if(arr[i] > arr[i+1])
+            {
+                arr[i+1] += arr[i];
+                arr[i] = arr[i+1] - arr[i];
+                arr[i+1] -= arr[i]; 
+            }
+        }
+    }
+}
+
+int main(void)
+{
+    srand(0);
+    int arr[10] = {};
+    for(int i=0; i<sizeof(arr)/sizeof(int); i++)
+        arr[i] = rand() % 26;
+    std::cout << std::endl;
+    
+    for(int i=0; i<sizeof(arr)/sizeof(int); i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+
+    bubbleSort(arr, sizeof(arr)/sizeof(int));
+
+    for(int i=0; i<sizeof(arr)/sizeof(int); i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
 }
