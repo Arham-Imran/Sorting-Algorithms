88f7dc6718fa15ec69179cc5249efee31d2666f9
Arham_Imran <marhamimran.mts42@gmail.com>
2024-09-12 17:46:50 +0500

    implemented heap sort

--- main.cpp
+++ main.cpp
@@ -1,5 +1,38 @@
 #include <bits/stdc++.h>
 
+void maxHeapify(int* arr, int size, int nodeNum)
+{
+    int leftChild = 2*nodeNum + 1;
+    int rightChild = 2*nodeNum + 2;
+
+    if(leftChild < size && arr[leftChild] > arr[nodeNum] && (rightChild >= size || (arr[leftChild] >= arr[rightChild])))
+    {
+        std::swap(arr[leftChild], arr[nodeNum]);
+        nodeNum = leftChild;
+    }
+    else if(rightChild < size && arr[rightChild] > arr[nodeNum])
+    {
+        std::swap(arr[rightChild], arr[nodeNum]);
+        nodeNum = rightChild;
+    }
+
+    if(nodeNum == rightChild || nodeNum == leftChild)
+        maxHeapify(arr, size, nodeNum);
+
+}
+
+void heapSort(int* arr, int size)
+{
+    for(int i = (size/2)-1; i>=0; i--)
+        maxHeapify(arr, size, i);
+
+    for(int i=size-1; i>0; i--)
+    {
+        std::swap(arr[0], arr[i]);
+        maxHeapify(arr, i, 0);
+    }
+}
+
 int main(void)
 {
     srand(time(0));
@@ -11,6 +44,8 @@ int main(void)
     for(int i=0; i<sizeof(arr)/sizeof(int); i++)
         std::cout << arr[i] << " ";
     std::cout << std::endl;
+
+    heapSort(arr, sizeof(arr)/sizeof(int));
 
     for(int i=0; i<sizeof(arr)/sizeof(int); i++)
         std::cout << arr[i] << " ";
