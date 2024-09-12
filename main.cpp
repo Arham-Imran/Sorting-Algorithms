3384041c6ac4149672b8a0fe902cfb82ed73ffb5
Arham_Imran <marhamimran.mts42@gmail.com>
2024-09-12 12:08:10 +0500

    implemented quick sort

--- main.cpp
+++ main.cpp
@@ -1,5 +1,28 @@
 #include <bits/stdc++.h>
 
+void quickSort(int* arr, int low, int high)
+{
+    if((high - low) <= 1)
+        return;
+
+    int pivot = arr[high];
+    for(int i=low+1; i<=high; i++)
+    {
+        if(arr[i] < pivot)
+        {
+            int temp = arr[++low];
+            arr[low] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    int temp = arr[++low];
+    arr[low] = pivot;
+    arr[high] = temp;
+
+    quickSort(arr, -1, --low);
+    quickSort(arr, low+1, high);
+}
+
 int main(void)
 {
     srand(time(0));
@@ -11,6 +34,8 @@ int main(void)
     for(int i=0; i<sizeof(arr)/sizeof(int); i++)
         std::cout << arr[i] << " ";
     std::cout << std::endl;
+
+    quickSort(arr, -1, sizeof(arr)/sizeof(int) - 1);
 
     for(int i=0; i<sizeof(arr)/sizeof(int); i++)
         std::cout << arr[i] << " ";
